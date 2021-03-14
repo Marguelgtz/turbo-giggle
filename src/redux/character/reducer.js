804863d207623b2a50e88432339fbb594971a696
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2021-03-14 03:04:48 -0600

    debugging movement

--- src/redux/character/reducer.js
+++ src/redux/character/reducer.js
@@ -16,28 +16,29 @@ const charReducer = (state = initialState, action) => {
   console.log(action);
   switch (action.type) {
     case "move-front":
-      console.log("foward case");
+      console.log("foward case", state.charPos.x);
+      console.log("foward case updated", state.charPos.x++);
       return {
         ...state,
-        charPos: { ...state.charPos, x: state.charPos + 1 },
+        charPos: { ...state.charPos, x: state.charPos.x + 1 },
       };
     case "move-back":
       console.log("back case");
       return {
         ...state,
-        charPos: { ...state.charPos, x: state.charPos - 1 },
+        charPos: { ...state.charPos, x: state.charPos.x - 1 },
       };
     case "move-left":
       console.log("left case");
       return {
         ...state,
-        charPos: { ...state.charPos, z: state.charPos - 1 },
+        charPos: { ...state.charPos, z: state.charPos.z - 1 },
       };
     case "move-right":
       console.log("right case");
       return {
         ...state,
-        charPos: { ...state.charPos, z: state.charPos + 1 },
+        charPos: { ...state.charPos, z: state.charPos.z + 1 },
       };
     default:
       return state;
