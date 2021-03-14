e33b2999d0eddb0e1bd15142b3c3d358120bba2b
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2021-03-14 01:58:32 -0600

    setting pup redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import reportWebVitals from "./reportWebVitals";
+//REDUX
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import character from "./redux/character/reducer";
+import gameStatus from "./redux/gameStatus/reducer";
+
+const reducer = combineReducers({
+  drumsPosition: character,
+  gameStatus: gameStatus,
+});
+
+const newStore = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={newStore}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
