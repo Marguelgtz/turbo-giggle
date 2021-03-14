3a9fa964ba93ef87d558f70016c03b22a3672b67
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2021-03-14 00:51:05 -0600

    this took a bit longer than I thought

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "./App.css";
 import React, { useState, useCallback, useEffect } from "react";
 import { Canvas } from "react-three-fiber";
 import {
@@ -7,16 +8,22 @@ import {
   PerspectiveCamera,
 } from "@react-three/drei";
 
+//Components
+import Plane from "./components/plane";
+
 const App = () => {
   return (
-    <div>
-      <Canvas>
+    <div className="App">
+      <Canvas shadowMap style={{ background: "#87CEEB" }}>
         <PerspectiveCamera
           makeDefault // Main Camera
           position={[10, 0, 10]} // boilerplate position will do follow obj hook for position or through redux maybe
         >
           <mesh />
         </PerspectiveCamera>
+        <OrbitControls />
+        <ambientLight intensity={0.2} />
+        <Plane />
       </Canvas>
     </div>
   );
